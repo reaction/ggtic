e007d9aab64e88dd3717a85eeb14b6d436fc1c6d
Matt Strickling <mastersofevil@gmail.com>
2011-08-19 22:24:19 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ggtic::Application.config.secret_token = '4951d0f37ef54b64d286b1ae38909fb43f1edc230b247cb5262e533346e837ab6d7708990f02497f2f5a8e32ebb189923f14b3e9d08c07d859ade32a59c6428d'
